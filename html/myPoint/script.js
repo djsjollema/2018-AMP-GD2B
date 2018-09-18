b3c65d69f7d5157a24070376d69392971799a2a4
Jelle Sjollema <j.sjollema@ma-web.nl>
2018-09-18 16:06:08 +0200

    'convetti'

--- html/myPoint/script.js
+++ html/myPoint/script.js
@@ -5,11 +5,13 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 
-
-for (let i = 0; i < 100; i++) {
+function animate(){
+  requestAnimationFrame(animate);
   let myPoint = new Point(randomNumber(canvas.width),randomNumber(canvas.height),"#" + Math.floor(randomNumber(255*255*255)).toString(16));
   myPoint.draw();
 }
+
+animate();
 
 function randomNumber(max){
   return Math.random()*max;
