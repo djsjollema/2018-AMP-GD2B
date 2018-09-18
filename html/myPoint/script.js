7f9c27b2ce77f0f07e622edbaca2fe454606eae1
Jelle Sjollema <j.sjollema@ma-web.nl>
2018-09-18 15:05:33 +0200

    myPoint

--- html/myPoint/script.js
+++ html/myPoint/script.js
@@ -1,2 +1,11 @@
 const canvas = document.getElementById('canvas');
 const context = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let myPoint = new Point(100,200);
+myPoint.draw();
+
+let myOtherPoint = new Point(300,500);
+myOtherPoint.draw();
