ff935375ef9e47d1c907173b906ed10705b23a09
Jelle Sjollema <j.sjollema@ma-web.nl>
2018-09-19 13:55:18 +0200

    connectTheDots extra

--- html/connectTheDots/script.js
+++ html/connectTheDots/script.js
@@ -5,24 +5,28 @@ canvas.height = window.innerHeight;
 
 let dots = [];
 
-for (var i = 0; i < 8; i++) {
-  let dot = new Point(randomNumber(canvas.width),randomNumber(canvas.height),"#" + Math.floor(randomNumber(255*255*255)).toString(16));
-  dots.push(dot);
+for (var i = 0; i <4; i++) {
+  addPoint();
 }
 
-
 function animate(){
+  context.clearRect(0,0,canvas.width,canvas.height)
   requestAnimationFrame(animate);
+  if(Math.random()<0.01){
+      dots.splice(0,1)
+      addPoint();
+
+  }
+  context.fillStyle = "rgba(255,255,0,0.2)"
   context.beginPath();
   context.moveTo(dots[0].x,dots[0].y);
   for (var i = 0; i < dots.length; i++) {
     context.lineWidth = 2;
     context.lineTo(dots[i].x,dots[i].y);
-
   }
   context.closePath();
+  context.fill();
   context.stroke();
-
   for (var i = 0; i < dots.length; i++) {
     dots[i].draw();
     dots[i].printText(i);
@@ -33,4 +37,9 @@ animate();
 
 function randomNumber(max){
   return Math.random()*max;
+}
+
+function addPoint(){
+  let dot = new Point(randomNumber(canvas.width),randomNumber(canvas.height),"#" + Math.floor(randomNumber(255*255*255)).toString(16));
+  dots.push(dot);
 }
