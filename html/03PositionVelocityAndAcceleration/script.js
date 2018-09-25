ffcead160c7a49610a03bfdea74111569f147184
Jelle Sjollema <j.sjollema@ma-web.nl>
2018-09-25 14:45:17 +0200

    03_03

--- html/03PositionVelocityAndAcceleration/script.js
+++ html/03PositionVelocityAndAcceleration/script.js
@@ -3,12 +3,18 @@ const context = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-let point = new Point(200,300,10,"blue");
-point.draw(context);
+let kineticObject = {};
+
+function setUp(){
+  kineticObject.point = new Point(200,300,10,"blue");
+
+  animate()
+}
 
 function animate(){
   context.clearRect(0,0,canvas.width,canvas.height)
   requestAnimationFrame(animate);
+  kineticObject.point.draw(context);
 }
 
-//animate();
+setUp()
