871d54cbee2a6b48a9f2c1f8b51209007ac5095b
Jelle Sjollema <j.sjollema@ma-web.nl>
2018-10-02 15:38:16 +0200

    05_02

--- html/05spawningBalls/script.js
+++ html/05spawningBalls/script.js
@@ -5,23 +5,37 @@ canvas.height = window.innerHeight;
 
  let kineticObjects = [];
 
-for (let i = 0; i < 10; i++) {
-  let kineticObject = {};
-  kineticObject.ball = new Point(1000,100,10,"red");
-  kineticObject.pos = new Vector2d(getRandomNumber(canvas.width),100);
-  kineticObject.vel = new Vector2d(0,1);
-  kineticObjects.push(kineticObject);
-}
-
 function animate(){
   context.clearRect(0,0,canvas.width,canvas.height)
   requestAnimationFrame(animate);
+  if(Math.random()<0.1){
+    let kineticObject = {};
+    kineticObject.ball = new Point(0,0,30,"red");
+    kineticObject.pos = new Vector2d(getRandomNumber(canvas.width),-100);
+    kineticObject.vel = new Vector2d(0,0);
+    kineticObject.acc = new Vector2d(0,0.2);
+    kineticObjects.push(kineticObject);
+  }
   for (var i = 0; i < kineticObjects.length; i++) {
+    kineticObjects[i].vel.add(kineticObjects[i].acc);
     kineticObjects[i].pos.add(kineticObjects[i].vel);
     kineticObjects[i].ball.position(kineticObjects[i].pos)
     kineticObjects[i].ball.draw(context);
   }
+
+  for (var i = 0; i < kineticObjects.length; i++) {
+    if(kineticObjects[i].pos.dy > canvas.height + 100){
+      kineticObjects.splice(i,1)
+    }
+  }
 }
+
+document.addEventListener('mousedown',(evt)=>{
+  let mousePos = {};
+  mousePos.x = evt.clientX;
+  mousePos.y = evt.clientY
+  console.log(mousePos);
+})
 
 animate();
 
