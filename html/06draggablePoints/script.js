ec217ba53b8f08029d7ada98161726b0cac62fbb
Jelle Sjollema <j.sjollema@ma-web.nl>
2018-10-03 13:25:40 +0200

    06_02

--- html/06draggablePoints/script.js
+++ html/06draggablePoints/script.js
@@ -6,3 +6,12 @@ canvas.height = window.innerHeight;
 let dragPoint = new Point(200,200,20,"red");
 dragPoint.draw(context);
 dragPoint.drag();
+
+
+function animate(){
+  context.clearRect(0,0,canvas.width,canvas.height);
+  requestAnimationFrame(animate);
+  dragPoint.draw(context);
+}
+
+animate();
