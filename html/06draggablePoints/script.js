f11b4f0dcae08e78a7d7a924f95b716bd94f21cb
Jelle Sjollema <j.sjollema@ma-web.nl>
2018-10-03 13:42:51 +0200

    06_03

--- html/06draggablePoints/script.js
+++ html/06draggablePoints/script.js
@@ -7,11 +7,26 @@ let dragPoint = new Point(200,200,20,"red");
 dragPoint.draw(context);
 dragPoint.drag();
 
+let otherDragPoint = new Point(300,100,20,"blue");
+otherDragPoint.drag();
+
+let anOtherOtherPoint = new Point(800,400,20,"yellow");
+anOtherOtherPoint.drag();
+
 
 function animate(){
   context.clearRect(0,0,canvas.width,canvas.height);
   requestAnimationFrame(animate);
+  context.beginPath();
+  context.moveTo(dragPoint.x,dragPoint.y);
+  context.lineTo(otherDragPoint.x,otherDragPoint.y);
+  context.lineTo(anOtherOtherPoint.x,anOtherOtherPoint.y);
+  context.closePath();
+  context.stroke();
+
   dragPoint.draw(context);
+  otherDragPoint.draw(context);
+  anOtherOtherPoint.draw(context);
 }
 
 animate();
