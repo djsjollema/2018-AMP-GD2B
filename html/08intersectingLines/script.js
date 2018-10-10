9816d0acf773d3331da1d30a9ca25887b95fa950
Jelle Sjollema <j.sjollema@ma-web.nl>
2018-10-10 13:29:03 +0200

    08.03

--- html/08intersectingLines/script.js
+++ html/08intersectingLines/script.js
@@ -8,6 +8,8 @@ let B = new Point(600,300,20,"blue");
 let C = new Point(200,500,20,"green");
 let D = new Point(600,100,20,"orange");
 
+let S = new Point(0,0,10,"white");
+
 let l = new LinearFunction(1,1);
 let m = new LinearFunction(1,1);
 
@@ -17,16 +19,21 @@ function animate(){
   requestAnimationFrame(animate);
   context.clearRect(0,0,canvas.width,canvas.height);
 
-  l.defineLineWithTwoPoint(A,B);
+  l.defineLineWithTwoPoints(A,B);
   l.draw(context);
 
-  m.defineLineWithTwoPoint(C,D);
+  m.defineLineWithTwoPoints(C,D);
   m.draw(context);
-  
+
+  S.x = l.intersection(m).x;
+  S.y = l.intersection(m).y
+
   A.draw(context);
   B.draw(context);
   C.draw(context);
   D.draw(context);
+
+  S.draw(context);
 
   A.printText("A");
   B.printText("B");
