480e46b40a1d4f567bbd0aa4e383fc400645cca1
Jelle Sjollema <j.sjollema@ma-web.nl>
2018-11-14 11:40:47 +0100

    14_2

--- html/14myCar/script.js
+++ html/14myCar/script.js
@@ -10,6 +10,13 @@ background.src = "street.png";
 let frontWheel = new Image();
 frontWheel.src = "wheel.png";
 
+let backWheel = new Image();
+backWheel.src = "wheel.png";
+
+
+let car = new Image();
+car.src = "car.png";
+
 let rot = 0;
 
 let xpos = 0;
@@ -20,18 +27,40 @@ background.addEventListener('load',()=>{
 })
 
 
+addEventListener('keydown',(evt)=>{
+  switch (evt.key) {
+    case "ArrowRight":
+      xspeed+= 1;
+      break;
+      case "ArrowLeft":
+      xspeed -=1;
+      break;
+    default:
+  }
+})
+
 function animate(){
   requestAnimationFrame(animate);
   context.clearRect(0,0,canvas.width,canvas.height);
   context.drawImage(background,0,0);
+
+  context.drawImage(car,xpos,200);
+
   context.save();
   context.translate(xpos,500)
   context.rotate(rot);
   context.drawImage(frontWheel,-frontWheel.width/2,-frontWheel.height/2);
   context.restore();
+
+  context.save();
+  context.translate(xpos - 300,200)
+  context.rotate(rot);
+  context.drawImage(backWheel,-backWheel.width/2,-backWheel.height/2);
+  context.restore();
+
   rot += 0.1;
   xpos += xspeed;
   if(xpos>canvas.width){
-    xpos = 0;
+    xpos = -car.width;
   }
 }
