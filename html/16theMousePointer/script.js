6c7238396a92a8f521375dfd01dc440dadea048b
Jelle Sjollema <j.sjollema@ma-web.nl>
2018-11-28 16:04:49 +0100

    15_1

--- html/16theMousePointer/script.js
+++ html/16theMousePointer/script.js
@@ -2,6 +2,8 @@ const canvas = document.getElementById('canvas');
 const context = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+let temp = 0;
+let mouse = {};
 
 let midX = canvas.width/2;
 let midY = canvas.height/2;
@@ -9,6 +11,38 @@ let midY = canvas.height/2;
 function animate(){
   requestAnimationFrame(animate);
   context.clearRect(0,0,canvas.width,canvas.height);
+  context.save();
+  context.translate(midX,midY);
+  context.rotate(temp);
+  drawArrow();
+  context.restore();
+  temp += 0.1;
 }
 
 animate();
+
+addEventListener('mousemove',(evt)=>{
+  mouse.x = evt.clientX;
+  mouse.y = evt.clientY;
+})
+
+function drawArrow(){
+  let sh = 6;
+  let sw = 50;
+  let ah = 15;
+  let aw = 50;
+  context.fillStyle = "red";
+
+  context.beginPath();
+  context.moveTo(0,0);
+  context.lineTo(0,sh);
+  context.lineTo(sw,sh);
+  context.lineTo(sw,ah);
+  context.lineTo(sw+aw,0);
+  context.lineTo(sw,-ah);
+  context.lineTo(sw,-sh);
+  context.lineTo(0,-sh);
+  context.closePath();
+  context.stroke();
+  context.fill();
+}
