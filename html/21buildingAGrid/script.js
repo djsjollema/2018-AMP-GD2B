8ad81d3ae159cba3a5fbd6c142750fa2b4bc9c90
Jelle Sjollema <j.sjollema@ma-web.nl>
2019-01-10 14:00:53 +0100

    21_1

--- html/21buildingAGrid/script.js
+++ html/21buildingAGrid/script.js
@@ -3,15 +3,38 @@ const context = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+let startTime,currentTime,dt;
+let framerate = 5;
+let counter = 0;
+let sx,sy;
+let numOfCols = 10;
+let maxFrames = 30;
+
+
 
 function setup(){
+  startTime = new Date();
 
   animate();
 }
 
 function animate(){
   requestAnimationFrame(animate);
-  context.clearRect(0,0,canvas.width,canvas.height);
+  currentTime = new Date();
+  dt = currentTime - startTime;
+  if(dt > 1000/framerate){
+    context.clearRect(0,0,canvas.width,canvas.height);
+    startTime = new Date();
+    sx = counter % numOfCols * 100;
+    sy = Math.floor(counter/numOfCols) * 100;
+    //console.log(counter, sx);
+    context.fillRect(sx,sy,100,100);
+    counter++;
+    if(counter >= 30){
+      counter = 0;
+    }
+  }
+
 }
 
 setup();
