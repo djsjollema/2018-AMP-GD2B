d8c730c9aeb734e439bf5531a49c7032ff27728a
Jelle Sjollema <j.sjollema@ma-web.nl>
2019-02-26 11:26:20 +0100

    25_01

--- html/25reflectingLine/script.js
+++ html/25reflectingLine/script.js
@@ -6,18 +6,27 @@ canvas.height = window.innerHeight;
 let A,B,l,m,player;
 
 function setup(){
-  A = new Point(100,100,10,"red");
-  B = new Point(700,300,10,"blue");
+  A = new Point(100,100,10,"yellow");
+  B = new Point(700,300,10,"lightgreen");
+  l = new LinearFunction(1,1);
   A.drag();
   B.drag();
+  player = {};
+  player.position = new Vector2d(500,500);
+  player.point = new Point(player.position.dx,player.position.dy,20,"red")
   animate();
 }
 
 function animate(){
   requestAnimationFrame(animate);
   context.clearRect(0,0,canvas.width,canvas.height)
+
+  l.defineLineWithTwoPoints(A,B);
+  l.draw(context);
+
   A.draw(context);
   B.draw(context);
+  player.point.draw(context);
 }
 
 setup();
