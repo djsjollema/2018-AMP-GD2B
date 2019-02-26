a1ebe1e9647ac1599c095aff0f4539c1d0df860d
Jelle Sjollema <j.sjollema@ma-web.nl>
2019-02-26 11:12:43 +0100

    25reflectingLine

---
+++ html/25reflectingLine/script.js
@@ -0,0 +1,15 @@
+const canvas = document.getElementById('canvas');
+const context = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+
+function setup(){
+
+}
+
+function animate(){
+  requestAnimationFrame(animate);
+}
+
+setup();
