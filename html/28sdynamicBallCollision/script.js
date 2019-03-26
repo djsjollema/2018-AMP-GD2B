21269389f6cd7c9a15c64f308f774617cf6a585c
Jelle Sjollema <j.sjollema@ma-web.nl>
2019-03-26 11:51:45 +0100

    28_4

--- html/28sdynamicBallCollision/script.js
+++ html/28sdynamicBallCollision/script.js
@@ -9,15 +9,17 @@ let gameObj2 = {};
 function setup(){
   gameObj1.pos = new Vector2d(200,200);
   gameObj1.vel = new Vector2d(2,3);
-  gameObj1.point = new Point(0,0,100,"yellow");
+  gameObj1.point = new Point(0,0,150,"yellow");
   gameObj1.point.position(gameObj1.pos);
   gameObj1.rad = new Vector2d(1,1);
+  gameObj1.tan = new Vector2d(1,1);
 
   gameObj2.pos = new Vector2d(600,300);
   gameObj2.vel = new Vector2d(2,-3);
-  gameObj2.point = new Point(0,0,100,"pink");
+  gameObj2.point = new Point(0,0,150,"pink");
   gameObj2.point.position(gameObj2.pos);
   gameObj2.rad = new Vector2d(1,1);
+  gameObj2.tan = new Vector2d(1,1);
   animate();
 }
 
@@ -54,9 +56,26 @@ function animate(){
   gameObj1.vel.draw(context,gameObj1.point.x,gameObj1.point.y,50);
   gameObj2.vel.draw(context,gameObj2.point.x,gameObj2.point.y,50);
 
-  gameObj1.rad.draw(context,gameObj1.point.x,gameObj1.point.y,1)
-  gameObj2.rad.draw(context,gameObj2.point.x,gameObj2.point.y,1)
+  if(gameObj1.rad.r < gameObj1.point.r + gameObj2.point.r){
+    // magnitude rad vector = 1
+    gameObj1.rad.r = 1;
+    gameObj2.rad.r = 1;
 
+    // tan perpedicular to rad
+    gameObj1.tan.dx = gameObj1.rad.dy;
+    gameObj1.tan.dy = -gameObj1.rad.dx;
+
+    gameObj2.tan.dx = gameObj2.rad.dy;
+    gameObj2.tan.dy = -gameObj2.rad.dx;
+
+
+  }
+
+  gameObj1.rad.draw(context,gameObj1.point.x,gameObj1.point.y,1);
+  gameObj2.rad.draw(context,gameObj2.point.x,gameObj2.point.y,1);
+
+  gameObj1.tan.draw(context,gameObj1.point.x,gameObj1.point.y,1);
+  gameObj2.tan.draw(context,gameObj2.point.x,gameObj2.point.y,1);
 }
 
 setup();
