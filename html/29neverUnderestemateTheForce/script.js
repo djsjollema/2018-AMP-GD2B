548f138977ee75f09b63c69d748657d7a0bdf069
Jelle Sjollema <j.sjollema@ma-web.nl>
2019-04-09 15:06:16 +0200

    29_5

--- html/29neverUnderestemateTheForce/script.js
+++ html/29neverUnderestemateTheForce/script.js
@@ -10,10 +10,11 @@ let distanceEarthMoon = 1;
 function setup(){
   l = new LinearFunction(1,1);
   m = new LinearFunction(1,1);
+  n = new LinearFunction(1,1);
 
 
   earth.pos = new Vector2d(canvas.width/2,canvas.height/2);
-  earth.point = new Point(earth.pos.dx,earth.pos.dy,100,"rgba(0,0,255,0.1)");
+  earth.point = new Point(earth.pos.dx,earth.pos.dy,100,"rgba(0,0,255,0.01)");
   moon.pos = new Vector2d(400,200);
   moon.vel = new Vector2d(1,-3);
   moon.acc = new Vector2d(0,0);
@@ -28,21 +29,27 @@ function setup(){
 
 function animate(){
   requestAnimationFrame(animate);
+  l.defineLineWithTwoPoints(moon.point,earth.point);
+  m.defineLineWithTwoPoints(moon1.point,earth.point);
+  n.defineLineWithTwoPoints(moon.point,moon1.point);
+
   distanceEarthMoon = earth.point.distanceToAnOtherPoint(moon.point);
   distanceEarthMoon1 = earth.point.distanceToAnOtherPoint(moon1.point);
   //console.log(distanceEarthMoon);
   moon.acc.diffenceVector(moon.pos,earth.pos);
-  moon.acc.r = 5000/(distanceEarthMoon*distanceEarthMoon);
+  moon.acc.r = 10000/(distanceEarthMoon*distanceEarthMoon);
 
   moon1.acc.diffenceVector(moon1.pos,earth.pos);
-  moon1.acc.r = 5000/(distanceEarthMoon1*distanceEarthMoon1);
+  moon1.acc.r = 10000/(distanceEarthMoon1*distanceEarthMoon1);
 
-  l.defineLineWithTwoPoints(moon.point,earth.point);
-  m.defineLineWithTwoPoints(moon1.point,earth.point);
+
 
 
   context.fillStyle = "rgba(0,51,102,0.1)";
   context.fillRect(0,0,canvas.width,canvas.height);
+
+
+
   earth.point.draw(context);
 
   moon.vel.add(moon.acc);
@@ -55,8 +62,9 @@ function animate(){
   moon1.point.position(moon1.pos);
   moon1.point.draw(context);
 
-  l.draw(context,"white");
+  l.draw(context,"green");
   m.draw(context,"yellow");
+  n.draw(context,"white");
   // moon.acc.draw(context,moon.pos.dx,moon.pos.dy,1000,"white");
   // moon1.acc.draw(context,moon1.pos.dx,moon1.pos.dy,1000,"white");
 }
