33abef8f4b2e5e1abeb69fcce9d2a1b307affa05
Jelle Sjollema <j.sjollema@ma-web.nl>
2019-04-09 13:44:07 +0200

    29_3

--- html/29neverUnderestemateTheForce/script.js
+++ html/29neverUnderestemateTheForce/script.js
@@ -8,8 +8,8 @@ let distanceEarthMoon = 1;
 
 function setup(){
   earth.pos = new Vector2d(canvas.width/2,canvas.height/2);
-  earth.point = new Point(earth.pos.dx,earth.pos.dy,150,"blue");
-  moon.pos = new Vector2d(200,200);
+  earth.point = new Point(earth.pos.dx,earth.pos.dy,100,"blue");
+  moon.pos = new Vector2d(400,200);
   moon.vel = new Vector2d(1,-3);
   moon.acc = new Vector2d(0,0);
   moon.point = new Point(moon.pos.dx,moon.pos.dy,20,"white")
@@ -21,11 +21,11 @@ function animate(){
   distanceEarthMoon = earth.point.distanceToAnOtherPoint(moon.point);
   //console.log(distanceEarthMoon);
   moon.acc.diffenceVector(moon.pos,earth.pos);
-  moon.acc.r = 10000/(distanceEarthMoon*distanceEarthMoon);
+  moon.acc.r = 5000/(distanceEarthMoon*distanceEarthMoon);
 
 
 
-  context.fillStyle = "rgba(0,51,102,0)";
+  context.fillStyle = "rgba(0,51,102,0.1)";
   context.fillRect(0,0,canvas.width,canvas.height);
   earth.point.draw(context);
 
@@ -34,7 +34,7 @@ function animate(){
   moon.point.position(moon.pos);
   moon.point.draw(context);
 
-  moon.acc.draw(context,moon.pos.dx,moon.pos.dy,1,"white");
+  moon.acc.draw(context,moon.pos.dx,moon.pos.dy,1000,"white");
 }
 
 
